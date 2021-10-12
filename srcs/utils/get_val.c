a1291c53cf6049a73109e4b4c4af286bbefdd84f
Noah Addino <leslie@Leslie.local>
2021-10-12 02:17:18 +0200

    up

--- srcs/utils/get_val.c
+++
@@ -1,6 +0,0 @@
-#include "push_swap.h"
-
-unsigned int get_val(t_list *lst, int index)
-{
-	return (*(unsigned int*) ft_lstget(lst, index)->data);
-}
--- srcs/utils/init_stacks.c
+++
@@ -1,8 +0,0 @@
-#include "push_swap.h"
-
-void	init_stacks(t_stacks *stacks)
-{
-	stacks->a = NULL;
-	stacks->b = NULL;
-	stacks->size = 0;
-}
